b13195b65308b12ec7531d37a85605f0f359f5f4
marunagahideo <hidoooo@gmail.com>
2016-08-10 17:33:02 +0900

    ビルドのための設定をファイルに記述するように修正。npm scripts を修正。

---
+++ build.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * モジュールのロード
+ */
+const packager = require('electron-packager'),
+      pkg = require('./package.json');
+
+packager({
+
+  name: 'Browsersync Launcher',
+  dir: './',
+  out: './dist',
+  platform: 'darwin',
+  arch: 'x64',
+  version: '1.3.2',
+  overwrite: true,
+  asar: false,
+
+  // アプリのバージョン、コピーライト
+  'app-version': pkg['version'],
+  'app-copyright': `Copyright (C) ${pkg['author']}`
+
+}, (err, appPaths) => {// 完了時のコールバック
+    if (err) {
+      console.log(err);
+    }
+    console.log('Done: ' + appPaths);
+});
